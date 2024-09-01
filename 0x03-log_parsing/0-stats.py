baf59d5efee19b6a11b1014ca5389e8d4037195c
prosperReigns <prosperkinging7@gmail.com>
2024-09-01 04:54:43 -0700

    done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
-import os
+import re
+
 
 def initialize_log():
     status_code = [200, 300, 400, 401, 403, 404, 405, 500]
@@ -8,6 +9,7 @@ def initialize_log():
     log = {"file_size": 0, "code_list": {str(code): 0 for code in status_code}}
     print(f"{log = }")
     return log
+
 
 def parse_line(line, regex, log):
     match = regex.fullmatch(line)
@@ -17,10 +19,11 @@ def parse_line(line, regex, log):
 
         log["file_size"] += int(file_size)
 
-        if stst_code.isdecimal():
+        if stat_code.isdecimal():
             log["code_list"][stat_code] += 1
 
     return log
+
 
 def print_codes(log):
     print("file_size: {}".format(log["file_size"]))
@@ -28,8 +31,9 @@ def print_codes(log):
     sorted_code_list = sorted(log["code_list"])
 
     for code in sorted_code_list:
-    if log["code_list"][code]:
-    print(f"{code}: {log['code_list'][code]}")
+        if log["code_list"][code]:
+            print(f"{code}: {log['code_list'][code]}")
+
 
 def main():
     regex = re.compile(r'\d{1, 3}\.\d{1, 3}\.\d{1, 3}\.\d{1, 3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/200 HTTP/1.1" (.{5}) (\d+)')
@@ -37,14 +41,15 @@ def main():
 
     line_count = 0
     for line in sys.stdin:
-    line = line.strip()
+        line = line.strip()
 
     line_count += 1
 
     parsed_log = parse_line(line, regex, log)
 
     if line_count % 10 == 0:
-    print_codes(parsed_log)
+        print_codes(parsed_log)
+
 
 if __name__ == "__main__":
     main()
